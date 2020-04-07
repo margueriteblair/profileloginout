edaec996c4c21af7f4c34565c701d2157e7b3211
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-07 17:06:20 -0400

    created basic object and function

---
+++ profileloginandout.js
@@ -0,0 +1,40 @@
+let profileUsers = {
+    username: "",
+    usersOnline: [],
+    usersOffline: [],
+    location: "",
+    locationsOnline: [],
+    locationsOffline: [],
+    login: function() {
+        this.usersOnline.push(this.username);
+        this.locationsOnline.push(this.location)
+        console.log(`USERS LOGGED IN:`);
+        console.log(`${this.usersOnline}`);
+        console.log(`From: ${this.locationsOnline}`);
+    },
+    logout: function() {
+        this.usersOffline.push(this.username)
+        this.locationsOffline.push(this.location); //insert index of function to find index of logged out
+        console.log(`USERS LOGGED OUT:`);
+        console.log(`${this.usersOffline}`);
+        console.log(`From ${this.locationsOffline}`);
+        console.log(this.usersOnline.splice(this.usersOnline.indexOf(this.username), 1))
+        console.log(this.locationsOnline.splice(this.locationsOnline.indexOf(this.location), 1))
+    }
+}
+
+function users(username, location, loginout) {
+    profileUsers.username = username;
+    profileUsers.location = location;
+    if (loginout == "in") {
+        profileUsers.login();
+    } else if (loginout == "out") {
+        profileUsers.logout( );
+    }
+}
+users("Chelsea", "Providence", "in");
+users("Margie", "Boston", "in");
+users("Margie", "Boston", "out");
+users("Jamie", "Colorado", "in");
+users("Chelsea", "Providence", "out");
+users("Arnell", "NYC", "in");
