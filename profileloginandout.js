bca504cae82b7e6ea45676b1488232c7b1eaa14e
Marguerite Blair <marguerite.blair@gmail.com>
2020-04-11 17:01:40 -0400

    added already logged in messages

--- profileloginandout.js
+++ profileloginandout.js
@@ -6,25 +6,31 @@ let profileUsers = {
     locationsOnline: [],
     locationsOffline: [],
     login: function() {
+        if (this.usersOnline.includes(this.username)) {
+            console.log(`----WARNING! THIS USER ${this.username} ALREADY LOGGED IN----`);
+        } else {
         this.usersOnline.push(this.username);
         this.locationsOnline.push(this.location)
         console.log(`USERS LOGGED IN:`);
         console.log(`${this.usersOnline}`);
         console.log(`From: ${this.locationsOnline}`);
-    },
+    } },
     logout: function() {
+        if (this.usersOffline.includes(this.username)) {
+            console.log(`----WARNING! THIS USER ${this.username} ALREADY LOGGED OFF----`)
+        } else {
         this.usersOffline.push(this.username)
-        this.locationsOffline.push(this.location); //insert index of function to find index of logged out
+        this.locationsOffline.push(this.location); 
         console.log(`USERS LOGGED OUT:`);
         console.log(`${this.usersOffline}`);
         console.log(`From ${this.locationsOffline}`);
         console.log(this.usersOnline.splice(this.usersOnline.indexOf(this.username), 1))
         console.log(this.locationsOnline.splice(this.locationsOnline.indexOf(this.location), 1))
-    }
+    } }
 }
 
 function users(username, location, loginout) {
-    profileUsers.username = username;
+    profileUsers.username = username; //usernames are distinct; no double registered usernames.
     profileUsers.location = location;
     if (loginout == "in") {
         profileUsers.login();
@@ -33,6 +39,7 @@ function users(username, location, loginout) {
     }
 }
 users("Chelsea", "Providence", "in");
+users("Margie", "Boston", "in");
 users("Margie", "Boston", "in");
 users("Margie", "Boston", "out");
 users("Jamie", "Colorado", "in");
